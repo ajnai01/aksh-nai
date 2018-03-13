30552f0d27533fab781eda8d139ce36ee4ebea8c
Nai <aksh.nai@stu.jefferson.kyschools.us>
2018-03-13 07:34:24 -0400

    may be my code is right

---
+++ python project.py
@@ -0,0 +1,11 @@
+#aksh_nayi-assign01.py
+media = input("what is the media ? ")
+Titles = input("what is the titles of your media? ")
+generes = str(input("what is the genres? "))
+years = input("which year it's realease? ")
+Description = input("what is description? ")
+Rating = float(500/100)
+
+new_list = [media,Titles,generes,years,Description,Rating]
+print (media,Titles,generes,years,Description,Rating)
+
